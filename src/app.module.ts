e2e8cd0bb780136e7677614c36e3c3682b0e1e28
Stefan Jaroni <sjaroni@outlook.de>
2025-03-31 22:05:03 +0200

    add: new resources users and tweet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TweetModule],
   controllers: [AppController],
   providers: [AppService],
 })
