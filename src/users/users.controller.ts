0ee35237a6d9a1cd5b9c87477f1c0992f3745328
Stefan Jaroni <sjaroni@outlook.de>
2025-04-01 20:26:33 +0200

    implement routes and methods, add logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    const usersService = new UsersService();
+    return usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  // @Param('id') id: number
+  getUserById(@Param('id') id: number) {
+    const usersService = new UsersService();
+    return usersService.getUserById(id);
+  }
+
+  @Post()
+  createUser(){
+    const user = {
+      id: 4,
+      name: 'Ron Weasley',      
+      age: 22,
+      gender: 'male',
+      isMarried: false
+    }
+    const usersService = new UsersService();
+    usersService.createUser(user);
+    return 'A new user has been created';
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    isMarried: boolean;
+  }[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      age: 30,
+      gender: 'male',
+      isMarried: false,
+    },
+    {
+      id: 2,
+      name: 'Mrs. J',
+      age: 25,
+      gender: 'female',
+      isMarried: false,
+    },
+    {
+      id: 3,
+      name: 'Harry Potter',
+      age: 25,
+      gender: 'male',
+      isMarried: true,
+    },
+  ];
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(user: {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    isMarried: boolean;
+  }) {
+    this.users.push(user);
+    return user;
+  }
+}
