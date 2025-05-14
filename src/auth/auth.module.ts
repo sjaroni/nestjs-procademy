e00bb0eefa0680c0216385084935fe75d1e84090
Stefan Jaroni <sjaroni@outlook.de>
2025-05-14 14:06:04 +0200

    feat: add bcrypt, add provider with abstract class for hashing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { HashingProvider } from './provider/hashing.provider';
+import { BcryptProvider } from './provider/bcrypt.provider';
 import authConfig from './config/auth.config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, {
+    provide: HashingProvider,
+    useClass: BcryptProvider,
+  }],
   imports: [
    // forwardRef(() => UsersModule), // nach Umstellung auf Authentifizierung keine Circular Dependency Injection mehr nötig
    UsersModule,
---
+++ src/auth/provider/bcrypt.provider.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { HashingProvider } from './hashing.provider';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptProvider implements HashingProvider {
+  public async hashPassword(password: string | Buffer): Promise<string> {
+    // Generate a salt
+    let salt = await bcrypt.genSalt();
+
+    // Hash the password
+    return await bcrypt.hash(password, salt);
+  }
+
+  public async comparePassword(
+    plainPassword: string | Buffer,
+    hashedPassword: string | Buffer,
+  ): Promise<boolean> {
+    return await bcrypt.compare(plainPassword, hashedPassword);
+  }
+}
