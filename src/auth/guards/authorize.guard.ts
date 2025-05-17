12ba1d76b892afa3ca510cac72d1783c4cbcc234
Stefan Jaroni <sjaroni@outlook.de>
2025-05-17 23:00:38 +0200

    feat: extract token from request

--- src/auth/guards/authorize.guard.ts
+++ src/auth/guards/authorize.guard.ts
@@ -4,6 +4,14 @@ import { Observable } from "rxjs";
 @Injectable()
 export class AuthorizeGuard implements CanActivate{
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    // Extract the request object from the context
+    const request = context.switchToHttp().getRequest();
+    
+    // Extract token from the request header
+    const token = request.headers['authorization']?.split(' ')[1];
+    // console.log(token);
+    // Validate token and provide / deny access
+
     return false;
   }
   
