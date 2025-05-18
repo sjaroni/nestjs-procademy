e4ca9eff0f7d252dd74fc339bd91878c963eb250
Stefan Jaroni <sjaroni@outlook.de>
2025-05-18 19:53:17 +0200

    feat: add validation of JSON Web Token

--- src/auth/guards/authorize.guard.ts
+++ src/auth/guards/authorize.guard.ts
@@ -1,18 +1,50 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Observable } from "rxjs";
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import authConfig from '../config/auth.config';
 
 @Injectable()
-export class AuthorizeGuard implements CanActivate{
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+export class AuthorizeGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(authConfig.KEY)
+    private readonly authConfiguration: ConfigType<typeof authConfig>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     // Extract the request object from the context
     const request = context.switchToHttp().getRequest();
-    
+
     // Extract token from the request header
     const token = request.headers['authorization']?.split(' ')[1];
-    // console.log(token);
+
     // Validate token and provide / deny access
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.authConfiguration        
+      );
+      
+      
+      request['user'] = payload;
+      console.log(payload);
+      
+
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
 
     return false;
   }
-  
 }
